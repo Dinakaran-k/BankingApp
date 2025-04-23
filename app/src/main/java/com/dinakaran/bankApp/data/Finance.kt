34dd62b2941573ef0777dd3889f0e2afe881f21f
Dinakaran <dinakarankommunuri@gmail.com>
2025-04-23 13:54:30 +0530

    feat: Remove unused imports in Finance.kt and Theme.kt
    Removed unused imports in Finance.kt and Theme.kt for better code cleanliness and efficiency.

--- app/src/main/java/com/dinakaran/bankApp/data/Finance.kt
+++ app/src/main/java/com/dinakaran/bankApp/data/Finance.kt
@@ -1,9 +1,5 @@
 package com.dinakaran.bankApp.data
 
-import android.content.ContentProvider
-import android.content.ContentValues
-import android.database.Cursor
-import android.net.Uri
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 
